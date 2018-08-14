61da8d3860c4a7f0ed8417bea5d20d2e98113dcd
Konrad Komorowski <konrad@komorowski.me>
2018-08-14 22:37:52 +0100

    copy account keys in `run.sh` everytime, make the command cross-platform

--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@
 IMG_NAME="daniel-jozsef/parityplayground"
 DATA_ROOT=${PWD}
 
+# restore account keys from the backup folder
+mkdir -p chainData/keys
+cp -Rv config/keysBkup/* chainData/keys/
+
 docker run \
   -p 8180:8180 -p 8545:8545 -p 8546:8546 -p 30303:30303 \
   -v $DATA_ROOT/chainData:/var/parity \
